d5f65f8ae1c82bf432c80732e83fabba869e382d
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-02-07 14:30:19 +0530

    buttonaction

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
   return (
-    <div id="main">
+    <div id="main" className="bg-pink">
+      <p><h1>This is a paragraph</h1></p>
+
+      <button id="button">Click Me</button>
       // Do not alter the main div
     </div>
   );
